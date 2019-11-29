a97093a2511088ed45ef97b210a1be51cbdc6e0c
Nguyen Quang Huy <nguyenquanghuy1997@gmail.com>
2019-11-29 18:42:47 +0300

    basic webpack + socketio + express + matterjs

---
+++ index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+
+var app = express();
+// Serve up content from public directory
+app.use(express.static(__dirname + '/dist'));
+// app.get('/', function (req, res) {
+//     res.sendFile(__dirname + '/index.html');
+//   });
+
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+io.on('connection', function (socket) {
+  socket.emit('hello');
+//   socket.on('my other event', function (data) {
+//     console.log(data);
+//   });
+});
+
+console.log("Server is listenning on port "+(process.env.PORT || 3000))
+
+server.listen(process.env.PORT || 3000);
