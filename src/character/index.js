1a6d267a6bfd6ee624ec807ed195775880f200c3
Nguyen Quang Huy <nguyenquanghuy1997@gmail.com>
2019-12-14 17:56:20 +0300

    update character

--- src/character/index.js
+++ src/character/index.js
@@ -1,59 +1,84 @@
 import Matter from 'matter-js'
 
 var Engine = Matter.Engine,
-    Render = Matter.Render,  
-    World = Matter.World,  
-    Bodies = Matter.Bodies,
-    Body = Matter.Body;
+	Render = Matter.Render,
+	Events = Matter.Events,
+	World = Matter.World,
+	Bodies = Matter.Bodies,
+	Body = Matter.Body;
 //class character
-export default class Character{
-	constructor(world,pos){
-			this.bodyC = Bodies.rectangle(pos.x,pos.y,50,50);
-			this.sensor = Bodies.rectangle(pos.x,pos.y+27,4,4, {isSensor: true});
-			this.composite = Body.create({
-				parts: [this.bodyC,this.sensor]
-			});
-			World.add(world,this.composite);
-			this.isJumping = true;
-			this.isChanneling = true; 
-			//create body
-			// Body.create();
+export default class Character {
+	constructor(engine, pos) {
+		this.bodyC = Bodies.rectangle(pos.x, pos.y, 50, 50, {inertia: Infinity});
+		this.sensor = Bodies.rectangle(pos.x, pos.y + 27, 4, 4, { isSensor: true });
+		this.composite = Body.create({
+			parts: [this.bodyC, this.sensor],
+			options: { objType: "character" }
+		});
+		// console.log(this.composite)
+		this.sensor.onCollideActive((pair) => {
+			//if (pair.bodyA.objType == "ground" || pair.bodyB.objType == "ground") {
+				// console.log(pairs)
+				this.isJumping = false;
+				// console.log("set to false")
+			//}
+		})
+		World.add(engine.world, this.composite);
+		// this.isJumping = true;
+		// this.isChanneling = true;
+		//create body
+		// Body.create();
+		this.maxJumpTime = 200;
 	}
-	move(e){
-		if(!this.isJumping){
-			//turn left non jump
-			if(e === 37){
-				Body.applyForce(this.composite,{x: this.bodyC.position.x,y:this.bodyC.position.y},{x:-0.02,y:0.00});
-			//move right not jump
-			}else if(e === 39){
-				Body.applyForce(this.composite,{x: this.bodyC.position.x,y:this.bodyC.position.y},{x:0.02,y:0.00});
-			}	
-		}else{
-			//turn left jumping
-			if(e === 37){
-				Body.applyForce(this.composite,{x: this.bodyC.position.x,y:this.bodyC.position.y},{x:-0.02,y:0.00});
-			//move right jumping
-			}else if(e === 39){
-				Body.applyForce(this.composite,{x: this.bodyC.position.x,y:this.bodyC.position.y},{x:0.02,y:0.00});
+	inputHandler(keyState) {
+		if (keyState[38]) {
+			this.jump();
+		}
+		if(keyState[37]) this.move(-1);
+		if(keyState[39]) this.move(1);
+	}
+	move(dir) {
+		let coeff = this.isJumping ? 0.1 : 1;
+		Body.applyForce(this.composite, { x: this.bodyC.position.x, y: this.bodyC.position.y }, { x: dir * 0.005 * coeff, y: 0.00 });
+
+		// if(!this.isJumping){
+		// 	//turn left non jump
+		// 	if(e === 37){
+		// 		Body.applyForce(this.composite,{x: this.bodyC.position.x,y:this.bodyC.position.y},{x:-0.02,y:0.00});
+		// 	//move right not jump
+		// 	}else if(e === 39){
+		// 		Body.applyForce(this.composite,{x: this.bodyC.position.x,y:this.bodyC.position.y},{x:0.02,y:0.00});
+		// 	}	
+		// }else{
+		// 	//turn left jumping
+		// 	if(e === 37){
+		// 		Body.applyForce(this.composite,{x: this.bodyC.position.x,y:this.bodyC.position.y},{x:-0.02,y:0.00});
+		// 	//move right jumping
+		// 	}else if(e === 39){
+		// 		Body.applyForce(this.composite,{x: this.bodyC.position.x,y:this.bodyC.position.y},{x:0.02,y:0.00});
+		// 	}
+		// }
+	}
+
+	jump() {
+		// console.log(this.isJumping);
+		console.log("called" + (!this.isJumping))
+		if (!this.isJumping) {
+			this.timeStartJump = new Date();
+			Body.applyForce(this.composite, { x: this.bodyC.position.x, y: this.bodyC.position.y }, { x: 0.00, y: -0.05 });
+			//set status in jump
+			this.isJumping = true;
+			// this.isChanneling = true;
+		} else {
+			if (new Date() - this.timeStartJump < this.maxJumpTime) {
+				Body.applyForce(this.composite, { x: this.bodyC.position.x, y: this.bodyC.position.y }, { x: 0.00, y: -0.003 });
 			}
 		}
 	}
-
-	jump(forceJump){
-		// console.log(this.isJumping);
-		if(!this.isJumping && !this.Channeling){
-		console.log(this.isJumping);
-		console.log("jumping");
-		Body.applyForce(this.composite,{x: this.bodyC.position.x,y:this.bodyC.position.y},{x:0.00,y:-forceJump});
-		//set status in jump
-		this.isJumping = true;
-		this.isChanneling = true;
-		}
-	}
-	effect(){
+	effect() {
 
 	}
-	sayHello(){
+	sayHello() {
 		console.log('hello');
 	}
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import io from 'socket.io-client'
 import Matter from 'matter-js'
+import { MatterCollisionEvents } from 'matter-collision-events';
 import Character from './character'
 
 var socket = io.connect();
 socket.on('hello', function () {
     console.log("connected!");
 });
+
+Matter.use(MatterCollisionEvents);
+
 var Engine = Matter.Engine,
-    Render = Matter.Render,  
+	Render = Matter.Render,
+	Events = Matter.Events,  
     World = Matter.World,  
     Bodies = Matter.Bodies,
     Body = Matter.Body;
@@ -23,15 +28,16 @@ var render = Render.create({
 
 // create two boxes and a ground
 var boxA = Bodies.rectangle(400, 200, 80, 80);
-var boxB = Bodies.rectangle(450, 50, 80, 80);
-var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
-var upBar = Bodies.rectangle(0, 0, 2000, 60, { isStatic: true });
-var rightBar = Bodies.rectangle(800, 400, 60, 810, { isStatic: true });
-var leftBar = Bodies.rectangle(0, 0, 60, 2000, { isStatic: true });
+var boxB = Bodies.rectangle(250, 50, 80, 80);
+var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true, objType:"ground" });
+console.log(ground)
+var upBar = Bodies.rectangle(0, 0, 2000, 60, { isStatic: true, objType:"ground" });
+var rightBar = Bodies.rectangle(800, 400, 60, 810, { isStatic: true, objType:"ground" });
+var leftBar = Bodies.rectangle(0, 0, 60, 2000, { isStatic: true, objType:"ground" });
 
 
 //create new character
-var character = new Character(engine.world,{x:500, y:500});
+var character = new Character(engine,{x:500, y:500});
 
 //variable for jump
 var timeStart = 0;
@@ -39,41 +45,19 @@ var timeEnd = 0;
 var deltaTime = 0;
 var forceJump = 0;
 
-//set status 
-	character.isJumping = false;
 
 //input to move character
-document.addEventListener('keyup', function(event) {
+var keyState = {}
+window.addEventListener('keydown',function(e){
+    keyState[e.keyCode || e.which] = true;
+},true);    
+window.addEventListener('keyup',function(e){
+    keyState[e.keyCode || e.which] = false;
+},true);
 
-	console.log(event.keyCode);
-	// Body.applyForce(boxA,{x:boxA.position.x,y:boxA.position.y},{x:0.0,y:-0.05});
-	//function move 
-	if(event.keyCode === 37||event.keyCode === 39){
-		character.move(event.keyCode);
-	}
-	//function jump
-	if(event.keyCode === 38){
-		var timeNow = new Date();
-		timeEnd = timeNow.getTime();
-		deltaTime = timeEnd - timeStart ;
-		console.log("time for force  = " +deltaTime);
-		forceJump = deltaTime/4000;
-		console.log("check");
-		character.jump(forceJump);
-		character.isJumping = false;
-		character.isChanneling = false;
-	}
-});	
-document.addEventListener('keydown',function(event){
-	//variable keydown
-	var timeStartPressDown = new Date(); //time start press
-	if(event.keyCode === 38){
-		var timeNow = new Date();
-		timeStart = timeNow.getTime();
-		// console.log(timeStart);
-	}
-});
-
+Events.on(engine, 'beforeUpdate', function(){
+	character.inputHandler(keyState)
+})
 // add all of the bodies to the world
 World.add(engine.world, [boxA, boxB, ground,leftBar,rightBar,upBar]);
 
