1e80253a26fae47bf544a74dd7c615041072122d
tuandung157 <letuandung157@gmail.com>
2020-01-03 22:41:30 +0300

    update file trap

---
+++ src/traps/BearTrap/index.js
@@ -0,0 +1,41 @@
+import Matter from 'matter-js'
+
+var Engine = Matter.Engine,
+	Render = Matter.Render,
+	Events = Matter.Events,
+	World = Matter.World,
+	Bodies = Matter.Bodies,
+	Body = Matter.Body;
+
+//class trap
+export default class Trap{
+
+  constructor(engine, pos){
+    this.bodyC = Bodies.rectangle(pos.x, pos.y, 50, 10, { inertia: Infinity ,objType: "bearTrap" });
+    this.sensorUp = Bodies.rectangle(pos.x, pos.y-5,40,0.02,{ isSensor: true});
+    this.composite = Body.create({
+      parts: [this.bodyC, this.sensorUp],
+      options: {objType: "bearTrap"}
+
+    });
+    this.engine = engine;
+
+    World.add(engine.world, this.composite);
+  }
+
+  sayHello() {
+    console.log('hello');
+  }
+	update() {
+		Matter.Query.collides(this.sensorUp, this.engine.world.bodies)
+		.forEach((collision) => {
+			// console.log(collision.bodyA.objType);
+			// console.log(collision.bodyB.objType);
+
+			if(collision.bodyA.objType == 'character' || collision.bodyB.objType == 'character') {
+				console.log(collision);
+				console.log("traped");
+			}
+		})
+	}
+}
