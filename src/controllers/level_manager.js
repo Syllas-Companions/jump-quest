e724691670e1acae964f0cc110a9d9d1bca4081e
unknown <letuandung157@gmail.com>
2020-03-12 19:12:41 +0700

    add file demo

--- src/controllers/level_manager.js
+++ src/controllers/level_manager.js
@@ -1,6 +1,6 @@
 export default {
     levels: new Map([
-        ['lobby', "maplevel1.json"],
+        ['lobby', "demo.json"],
         ['0', "maplevel1.json"],
         ['1', "maplevel2.json"],
         ['2', "maplevel3.json"]
